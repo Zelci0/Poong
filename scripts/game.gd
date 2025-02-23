496a0bae4825dfd84be07acb826c97d8cd732e2d
Zelci0 <joserraimundo97@outlook.com>
2025-02-23 20:57:42 -0300

    Asteroids added (no collision)
    Added:
    
    + Added asteroids to the game (no collision detection yet).
    
    Changes:
    
    * Renamed project from "Space Pong" to "Poong!".
    
    Fixes:
    
    - Removed unused parameters ("delta" and "body") in functions to clean up code and resolve debugger error.

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,8 +3,11 @@ extends Node2D
 @onready var ball = $Ball
 @onready var label_score = $Score
 @onready var label_tutorial = $Tutorial
+@onready var positions = $Positions
+var last_position
+var asteroid_scene = preload("res://scenes/asteroide.tscn")
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	if ball.started:
 		label_score.visible = true
@@ -13,5 +16,28 @@ func _process(delta: float) -> void:
 	label_score.text = str(ball.score)
 
 
-func _on_dead_zone_body_entered(body: Node2D) -> void:
+func _on_dead_zone_body_entered(_body: Node2D) -> void:
+	call_deferred("reload_scene")
+
+func reload_scene():
 	get_tree().reload_current_scene()
+
+
+func _on_timer_spawner_timeout() -> void:
+	#Gera os asteróides quando o timer chega a 0
+	spawn_asteroid()
+	
+	
+func spawn_asteroid():
+	#Gera os asteróides
+	if (ball.started):
+		var positions_list = positions.get_children()
+		var spawn_position = positions_list.pick_random()
+		
+		if (spawn_position != last_position):
+			var asteroide_instance = asteroid_scene.instantiate()
+			asteroide_instance.global_position = spawn_position.position
+			add_child(asteroide_instance)
+			last_position = spawn_position
+		else:
+			spawn_asteroid()
